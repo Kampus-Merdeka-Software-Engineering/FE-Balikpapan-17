f036a7352911ccae80cc7ff1754aa25b064a7dfa
reginapramn <adistyreginapramn@gmail.com>
2023-09-14 22:48:23 +0700

    tambahan faq dan fitur di bawah resi

---
+++ faq/script.js
@@ -0,0 +1,13 @@
+// toggle class active
+const navbarNav = document.querySelector(".navbar-nav");
+
+document.querySelector("#hamburger-menu").onclick = () => {
+  navbarNav.classList.toggle("active");
+};
+
+const hamburger = document.querySelector("#hamburger-menu");
+
+document.addEventListener("click", function (e) {
+  if (!hamburger.contains(e.target) && !navbarNav.contains(e.target))
+    navbarNav.classList.remove("active");
+});
