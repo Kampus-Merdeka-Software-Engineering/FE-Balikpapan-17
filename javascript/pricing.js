6bbb8c4eaae0a37ee5b4f22a3076b0d09a030a05
Panca Rulian Effendi <effendi.rulian@gmail.com>
2023-10-02 20:38:59 +0700

    anchor page

--- javascript/pricing.js
+++ javascript/pricing.js
@@ -1,6 +1,19 @@
 function displayResult() {
-    const dropdown = document.getElementById('dropdown');
-    const result = document.getElementById('result');
-    const selectedOption = dropdown.options[dropdown.selectedIndex].text;
-    result.innerText = `Anda memilih: ${selectedOption}`;
+  const dropdown = document.getElementById("dropdown");
+  const result = document.getElementById("result");
+  const selectedOption = dropdown.options[dropdown.selectedIndex].text;
+  result.innerText = `Anda memilih: ${selectedOption}`;
 }
+
+const navbarNav = document.querySelector(".navbar-nav");
+
+document.querySelector("#hamburger-menu").onclick = () => {
+  navbarNav.classList.toggle("active");
+};
+
+const hamburger = document.querySelector("#hamburger-menu");
+
+document.addEventListener("click", function (e) {
+  if (!hamburger.contains(e.target) && !navbarNav.contains(e.target))
+    navbarNav.classList.remove("active");
+});
