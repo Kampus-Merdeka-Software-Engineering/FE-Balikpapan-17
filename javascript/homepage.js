d7978cd234b772b2173f2696cb1e27ee30d6f92d
reginapramn <adistyreginapramn@gmail.com>
2023-10-06 22:15:59 +0700

    fetch resi javascript

--- javascript/homepage.js
+++ javascript/homepage.js
@@ -1,3 +1,5 @@
+const API_URL = "http://localhost:3001"
+
 // toggle class active
 const navbarNav = document.querySelector(".navbar-nav");
 
@@ -51,3 +53,19 @@ carousel.addEventListener("mousedown", dragStart);
 carousel.addEventListener("mousemove", dragging);
 document.addEventListener("mouseup", dragStop);
 
+async function fetchResiByNoResi() {
+  const resiId = document.getElementById('resicheck').value;
+  try {
+      const response = await fetch(`${API_URL}/resi/${resiId}`);
+      const Resi = await response.json();
+      const resiDetails = document.getElementById('resi-details');
+      resiDetails.innerHTML = `Nomor Resi Anda: ${Resi.resi}<br>Berat Paket: ${Resi.berat}<br>
+      Nama Pengirim: ${Resi.namapengirim}<br>Alamat Pengirim: ${Resi.alamatpengirim}<br>No.Telp Pengirim: ${Resi.telp_pengirim}<br>
+      Nama Penerima: ${Resi.namapenerima}<br>Alamat Penerima: ${Resi.alamatpenerima}<br>No.Telp Penerima: ${Resi.telp_penerima}`;
+      
+  } catch (error) {
+      console.error('Error fetching resi:', error);
+  }
+}
+
+// fetchResi();
